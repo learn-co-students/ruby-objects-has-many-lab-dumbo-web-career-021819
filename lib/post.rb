8744822ea423a43e7b9a8258f410f31aa03b72ba
1-8192 <allegranzia@gmail.com>
2019-02-20 22:21:53 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+class Post
+
+attr_accessor :title, :author
+@@all = []
+
+def self.all
+  @@all
+end
+
+def initialize(title)
+  @title = title
+  @@all << self
+end
+
+def author_name
+    if defined? @author != nil
+      @author.name
+    else
+      nil
+    end 
+end
+
+end
